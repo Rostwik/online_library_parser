5c2adedbd6cfc616e058b18b260d34b75438d598
Rostislav <rostwik@gmail.com>
2022-11-17 00:54:59 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
-import requests
 import os
+
+import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
+
+def download_txt(url, filename, folder='books/'):
+    os.makedirs(folder, exist_ok=True)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+    return filepath
+
+
+def check_for_redirect(response):
+    if response.history:
+        if response.url == 'https://tululu.org/':
+            raise requests.HTTPError(response.url)
 
 
 def main():
-    os.makedirs('books', exist_ok=True)
+    site_url = 'https://tululu.org/'
 
     for id in range(10):
-        url = f'https://tululu.org/txt.php?id={id + 1}'
-        print(url)
+        try:
+            title_url = f'{site_url}b{id + 1}'
+            print(title_url)
+            response = requests.get(title_url)
+            response.raise_for_status()
+            check_for_redirect(response)
 
-        response = requests.get(url)
-        response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'lxml')
+            book_tag = soup.find('h1').text
+            title, _ = book_tag.split('::')
 
-        filename = f'books/book{id}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+            download_url = f'{site_url}txt.php?id={id + 1}'
+            unique_title = f'{id + 1}. {title.strip()}'
+
+            filepath = download_txt(download_url, unique_title)
+            print(filepath)
+
+        except requests.HTTPError as exp:
+            print(exp)
+
+    # print(soup.find('img', class_='attachment-post-image')['src'])
+    # title_tag_post = soup.find('div', class_='entry-content')
+    # print(title_tag_post.text)
 
 
 if __name__ == '__main__':
