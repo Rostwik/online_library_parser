7fc042b805d2df6f2ebf4fb539932571541f5ee8
Rostislav <rostwik@gmail.com>
2022-11-18 21:59:58 +0300

    Download book covers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlparse, urlsplit
 
 
 def download_txt(url, filename, folder='books/'):
@@ -12,6 +13,22 @@ def download_txt(url, filename, folder='books/'):
     response.raise_for_status()
 
     filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+    return filepath
+
+
+def download_image(url, folder='images/'):
+    os.makedirs(folder, exist_ok=True)
+
+    filename = urlsplit(url).path.split('/')[-1]
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    filepath = os.path.join(folder, filename)
 
     with open(filepath, 'wb') as file:
         file.write(response.content)
@@ -38,6 +55,7 @@ def main():
 
             soup = BeautifulSoup(response.text, 'lxml')
             book_tag = soup.find('h1').text
+            img_tag = soup.find(class_='bookimage').find('img')['src']
             title, _ = book_tag.split('::')
 
             download_url = f'{site_url}txt.php?id={id + 1}'
@@ -46,12 +64,12 @@ def main():
             filepath = download_txt(download_url, unique_title)
             print(filepath)
 
+            filepath = download_image(urljoin(site_url, img_tag))
+            print(filepath)
+
+
         except requests.HTTPError as exp:
             print(exp)
-
-    # print(soup.find('img', class_='attachment-post-image')['src'])
-    # title_tag_post = soup.find('div', class_='entry-content')
-    # print(title_tag_post.text)
 
 
 if __name__ == '__main__':
