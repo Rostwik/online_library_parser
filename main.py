fbbc31b3746ab8bd2d696f52cd6a32c886b2d4fe
Rostislav <rostwik@gmail.com>
2022-11-19 19:52:14 +0300

    Add parser of page

--- main.py
+++ main.py
@@ -6,6 +6,29 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlparse, urlsplit
 
 
+def parse_book_page(soup):
+    book_info = dict()
+
+    book_info['img_url'] = soup.find(class_='bookimage').find('img')['src']
+
+    book_tag = soup.find('h1').text
+    title, author = book_tag.split('::')
+    book_info['title'] = title.strip()
+    book_info['author'] = author.strip()
+
+    comment_tag = soup.find_all(class_='texts')
+    if comment_tag:
+        book_comments = '\n'.join([comment.find(class_='black').text for comment in comment_tag])
+        book_info['book_comments'] = book_comments
+
+    genre_tag = soup.find('span', class_='d_book')
+    if genre_tag:
+        genre_list = [genre.text for genre in genre_tag.find_all('a')]
+        book_info['genres'] = genre_list
+
+
+    print(book_info)
+
 def download_txt(url, filename, folder='books/'):
     os.makedirs(folder, exist_ok=True)
 
@@ -63,30 +86,14 @@ def main():
             check_for_redirect(response)
 
             soup = BeautifulSoup(response.text, 'lxml')
-            book_tag = soup.find('h1').text
-            img_tag = soup.find(class_='bookimage').find('img')['src']
-
-            title, _ = book_tag.split('::')
-            download_url = f'{site_url}txt.php?id={id + 1}'
-            unique_title = f'{id + 1}. {title.strip()}'
-
-            book_filepath = download_txt(download_url, unique_title)
-
-            comment_tag = soup.find_all(class_='texts')
-            if comment_tag:
-                book_comments = '\n'.join([comment.find(class_='black').text for comment in comment_tag])
-                download_comments(book_comments, unique_title)
-
-            genre_tag = soup.find('span', class_='d_book')
-            if genre_tag:
-                genre_list = [genre.text for genre in genre_tag.find_all('a')]
-                print(f'Заголовок: {unique_title}')
-                print(genre_list)
-
-            #
-            # filepath = download_image(urljoin(site_url, img_tag))
-            # print(filepath)
-
+            parse_book_page(soup)
+            # book_info['unique_title'] = f'{id + 1}. {title.strip()}'
+            # download_url = f'{site_url}txt.php?id={id + 1}'
+            # book_filepath = download_txt(download_url, unique_title)
+            # #
+            # # filepath = download_image(urljoin(site_url, img_tag))
+            # # print(filepath)
+            # download_comments(book_comments, unique_title)
 
         except requests.HTTPError as exp:
             print(exp)
