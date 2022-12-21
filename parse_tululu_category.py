aa03f8b8ed7af08112f688f521aeb237a6868257
Rostislav <rostwik@gmail.com>
2022-12-21 20:46:04 +0300

    Enabled the ability to select a range

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,14 +1,29 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse, urlsplit
 from main import check_for_redirect, download_txt, download_image, download_comments, parse_book_page
 import json
+import argparse
 
 url = 'https://tululu.org/l55/'
 
-for page in range(2):
+parser = argparse.ArgumentParser(description='''Программа скачивает книги в формате txt,'
+                                                 а также обложки и комментарии в отдельные папки.
+                                                 Формирует json файл с данными скачиваемых книг и сохраняет его
+                                                 в указанной директории.
+                                                 Для работы программы в командной строке необходимо ввести
+                                                 два обязательных параметра: номер страницы начала и номер страницы
+                                                 окончания интервала скачиваемых книг.                                                 
+                                                 ''')
+parser.add_argument('--start_page', help='Начало интервала', type=int, default=1)
+parser.add_argument('--end_page', help='Конец интервала', type=int, default=10)
+args = parser.parse_args()
 
-    page_url = urljoin(url, str(page + 1))
+for page in range(args.start_page, args.end_page):
+
+    page_url = urljoin(url, str(page))
     response = requests.get(page_url)
     response.raise_for_status()
 
@@ -34,8 +49,6 @@ for page in range(2):
             book_download_attributes['book_path'] = download_txt(txt_url, book_number, unique_title)
 
             book_download_attributes['img_url'] = download_image(urljoin(book_url, book_download_attributes['img_url']))
-
-            print(book_download_attributes)
 
             books_json = json.dumps(book_download_attributes, ensure_ascii=False)
 
